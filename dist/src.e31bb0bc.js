7b076e764d157a5f7c65b1e933008be8a22d3b3d
Roy Jay <rj@arcamax.com>
2020-07-27 09:08:25 -0400

    test2

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -31899,7 +31899,7 @@ function App() {
   }, 0);
   document.title = "".concat(hazards, " potential HAZARDS ").concat(hazards > 0 ? "😱" : "👍");
   var results = data.result.near_earth_objects[day];
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, (0, _format.default)((0, _addDays.default)(new Date(), 1), "EEEE d-MMM"), " there will be", " ", /*#__PURE__*/_react.default.createElement("strong", null, results.length), " near misses"), /*#__PURE__*/_react.default.createElement("hr", null), results.sort(function (a) {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, (0, _format.default)((0, _addDays.default)(new Date(), 1), "EEEE d-MMM"), " there will be", " ", /*#__PURE__*/_react.default.createElement("strong", null, results.length), " pigs fly"), /*#__PURE__*/_react.default.createElement("hr", null), results.sort(function (a) {
     return a.is_potentially_hazardous_asteroid ? -1 : 1;
   }).map(function (data) {
     return /*#__PURE__*/_react.default.createElement(_Orbital.default, _extends({
