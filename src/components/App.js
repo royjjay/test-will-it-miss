3a5f5712c14f0cd2156e30abcc4abf979497c864
Roy Jay <rj@arcamax.com>
2020-07-27 09:17:37 -0400

    test3

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ export default function App() {
     <div>
       <p>
         {format(addDays(new Date(), 1), "EEEE d-MMM")} there will be{" "}
-        <strong>{results.length}</strong> pigs will fly
+        <strong>{results.length}</strong> pigs will fly home
       </p>
       <hr></hr>
       {results
